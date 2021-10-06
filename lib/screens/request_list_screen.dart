170c7e5c188b8224280d097545f026e09d4cd48c
Hao <haodngn@gmail.com>
2021-10-07 04:04:54 +0700

    hot fix 710

--- lib/screens/request_list_screen.dart
+++ lib/screens/request_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:it_support/constant.dart';
+import 'package:it_support/screens/bottom_nav_bar_screen.dart';
 import 'package:it_support/screens/components/detail_card.dart';
 
 class ListRequestScreen extends StatelessWidget {
@@ -8,6 +9,7 @@ class ListRequestScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Danh Sách Yêu Cầu"),
+        automaticallyImplyLeading: false,
         backgroundColor: kBlueColor,
       ),
       backgroundColor: kBackgroundColor,
@@ -15,16 +17,41 @@ class ListRequestScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               SizedBox(
                 height: 140,
               ),
               buildDetailList(),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(120, 50, 0, 0),
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: kBlueColor,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: new FlatButton(
+                    child: new Text("Quay về trang chủ", style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: kWhiteColor,
+                    ),),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: backtoHomePage));
+                    },
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+  Widget backtoHomePage(BuildContext context) {
+    return BottomNavScreen();
   }
 
   buildDetailList() {
@@ -44,7 +71,7 @@ class ListRequestScreen extends StatelessWidget {
             height: 20,
           ),
           DetailCard(
-            'Nguyễn Văn B',
+            'Nguyễn Văn A',
             'Thiết bị : Iphone 13 pro max',
             'assets/images/Yeti.png',
             kYellowColor,
@@ -53,7 +80,7 @@ class ListRequestScreen extends StatelessWidget {
             height: 20,
           ),
           DetailCard(
-            'Nguyễn Văn C',
+            'Nguyễn Văn A',
             'Thiết bị : PC',
             'assets/images/Yeti.png',
             kOrangeColor,
