431aebb025f58afc5106296b8696039ca5c1ff7d
Hao <haodngn@gmail.com>
2021-10-07 15:24:41 +0700

    test merge request

---
+++ lib/screens/new_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NewScreen extends StatelessWidget {
+  const NewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("New Screen"),
+      ),
+    );
+  }
+}
