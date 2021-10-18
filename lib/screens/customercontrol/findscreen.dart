9e31c81489900402c9f47b01982d22c3bd2d5acf
Nham Duc Dat <datndse141132@fpt.edu.vn>
2021-10-19 00:19:56 +0700

    timer

--- lib/screens/customercontrol/findscreen.dart
+++ lib/screens/customercontrol/findscreen.dart
@@ -1,12 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:it_support/screens/customercontrol/fixingscreen.dart';
+
 class findscreen extends StatefulWidget {
-  const findscreen({ Key? key }) : super(key: key);
+  const findscreen({Key? key}) : super(key: key);
 
   @override
   _findscreenState createState() => _findscreenState();
 }
 
 class _findscreenState extends State<findscreen> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 4), () {
+      Navigator.push(
+    context, MaterialPageRoute(builder: (context) => fixingscreen()));
+    });
+
+    super.initState();
+  }
+
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +37,7 @@ class _findscreenState extends State<findscreen> {
   }
 }
 
-class Body  extends StatefulWidget {
+class Body extends StatefulWidget {
   @override
   _State createState() {
     return _State();
@@ -32,23 +48,23 @@ class _State extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20,180,20,20),
-        child: Container(
-          alignment: Alignment.bottomCenter,
-          width: 355,
-          height: 300,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(30, 50, 30, 140),
-            child: Text("Đang tìm kiếm người sửa....", style: TextStyle(fontSize: 23),),
-          ),      
-          decoration: BoxDecoration(
-            color: Colors.white,            
-            borderRadius: BorderRadius.circular(29)
+        backgroundColor: Colors.grey,
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 180, 20, 20),
+          child: Container(
+            alignment: Alignment.bottomCenter,
+            width: 355,
+            height: 300,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(30, 50, 30, 140),
+              child: Text(
+                "Đang tìm kiếm người sửa....",
+                style: TextStyle(fontSize: 23),
+              ),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(29)),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
--- lib/screens/customercontrol/fixingscreen.dart
+++ lib/screens/customercontrol/fixingscreen.dart
@@ -1,12 +1,26 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:it_support/screens/customercontrol/billscreen.dart';
+
 class fixingscreen extends StatefulWidget {
-  const fixingscreen({ Key? key }) : super(key: key);
+  const fixingscreen({Key? key}) : super(key: key);
 
   @override
   _fixingscreenState createState() => _fixingscreenState();
 }
 
 class _fixingscreenState extends State<fixingscreen> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 4), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => billscreen()));
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +36,7 @@ class _fixingscreenState extends State<fixingscreen> {
 }
 
 class Body extends StatefulWidget {
-  const Body({ Key? key }) : super(key: key);
+  const Body({Key? key}) : super(key: key);
 
   @override
   _BodyState createState() => _BodyState();
@@ -32,23 +46,23 @@ class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20,180,20,20),
-        child: Container(
-          alignment: Alignment.bottomCenter,
-          width: 355,
-          height: 300,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(30, 50, 30, 140),
-            child: Text("Đang sửa chữa thiết bị....", style: TextStyle(fontSize: 23),),
-          ),      
-          decoration: BoxDecoration(
-            color: Colors.white,            
-            borderRadius: BorderRadius.circular(29)
+        backgroundColor: Colors.grey,
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 180, 20, 20),
+          child: Container(
+            alignment: Alignment.bottomCenter,
+            width: 355,
+            height: 300,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(30, 50, 30, 140),
+              child: Text(
+                "Đang sửa chữa thiết bị....",
+                style: TextStyle(fontSize: 23),
+              ),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(29)),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
